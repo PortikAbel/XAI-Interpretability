5c1fcb61ebd15c57fd4bf1c61b9a6d5204aa5ee7
Portik Abel <portikusz0@gmail.com>
2024-04-05 19:03:32 +0300

    parameter grouping by dictionary containing layer grouping rule

--- src/models/features/__init__.py
+++ src/models/features/__init__.py
@@ -39,3 +39,62 @@ base_architecture_to_features = {
     "vgg19": vgg19_features,
     "vgg19_bn": vgg19_bn_features,
 }
+
+
+base_architecture_to_layer_groups = {
+    "convnext_tiny_13": {
+        "to_train": ["features.5.3"],
+        "to_freeze": ["features.5", "features.4"],
+        "backbone": ['features.3', 'features.2'],
+    },
+    "convnext_tiny_26": {
+        "to_train": ["features.7.2"],
+        "to_freeze": ["features.7", "features.6"],
+        "backbone": ['features.5', 'features.4'],
+    },
+    "resnet18": {
+        "to_train": ["layer4.1"],
+        "to_freeze": ["layer4", "layer3"],
+        "backbone": ["layer2"],
+    },
+    "resnet34": {
+        "to_train": ["layer4.1"],
+        "to_freeze": ["layer4", "layer3"],
+        "backbone": ["layer2"],
+    },
+    "resnet50": {
+        "to_train": ["layer4.2"],
+        "to_freeze": ["layer4", "layer3"],
+        "backbone": ["layer2"],
+    },
+    "resnet101": {
+        "to_train": ["layer3.2"],
+        "to_freeze": ["layer3"],
+        "backbone": ["layer2"],
+    },
+    "resnet152": {
+        "to_train": ["layer2.7"],
+        "to_freeze": ["layer2.6", "layer2.5", "layer2.4"],
+        "backbone": ["layer2"],
+    },
+    "vgg11": {
+        "to_train": ["features18"],
+        "to_freeze": ["features16", "features13", "features11"],
+        "backbone": ["features8", "features6"],
+    },
+    "vgg13": {
+        "to_train": ["features22"],
+        "to_freeze": ["features20", "features17", "features15"],
+        "backbone": ["features12", "features10", "features7"],
+    },
+    "vgg16": {
+        "to_train": ["features28"],
+        "to_freeze": ["features26", "features24", "features21"],
+        "backbone": ["features19", "features17", "features14"],
+    },
+    "vgg19": {
+        "to_train": ["features34"],
+        "to_freeze": ["features32", "features30", "features28"],
+        "backbone": ["features25", "features23", "features21"],
+    },
+}
